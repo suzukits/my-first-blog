f6491138761ff7b4a2291aae0c55a4b783aa053c
suzukits <tacsol.suzukits@gmail.com>
2019-01-27 22:12:10 +0900

    last realy

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,8 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+#            return redirect('post_detail', pk=post.pk)
+            return render(request, 'blog/post_detail.html', {'post': post})
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
@@ -33,7 +34,8 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+#            return redirect('post_detail', pk=post.pk)
+            return render(request, 'blog/post_detail.html', {'post': post})
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
